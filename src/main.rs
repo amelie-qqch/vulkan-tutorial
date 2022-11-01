7fd0eab34ad24f596033437e6fbca9b616bdd211
ame <amedouillard@hotmail.fr>
2022-11-01 12:56:23 +0100

    Command buffers

--- src/main.rs
+++ src/main.rs
@@ -112,6 +112,8 @@ impl App {
         create_render_pass(&instance, &logical_device, &mut data);
         create_pipeline(&logical_device, &mut data)?;
         create_framebuffers(&logical_device, &mut data)?;
+        create_command_pool(&instance, &logical_device, &mut data)?;
+        create_command_buffers(&device, &mut data)?;
 
         Ok(Self { entry, instance, data, logical_device })
     }
@@ -124,6 +126,7 @@ impl App {
     /// Destroys Vulkan app
     #[rustfmt::skip]
     unsafe fn destroy(&mut self) {
+        self.logical_device.destroy_command_pool(self.data.command_pool, None);
         self.data.framebuffers
             .iter()
             .for_each(|f0| self.logical_device.destroy_framebuffer(*f, None));
@@ -226,6 +229,8 @@ struct AppData{
     pipeline_layout: vk::PipelineLayout,
     pipeline: vk::Pipeline,
     framebuffers: Vec<vk::Framebuffer>,
+    command_pool: vk::CommandPool,
+    command_buffers: Vec<vk::CommandBuffer>,
 }
 
 /////// LOGICAL DEVICE ///////
@@ -745,6 +750,79 @@ unsafe fn create_framebuffers(device: &Device, data: &mut AppData) -> Result<()>
 
         })
         .collect::<Result<Vec<_>, _>>()?;
+
+
+    Ok(())
+}
+
+/////// COMMAND BUFFERS ///////
+
+// Les command_pool gèrent la mémoire utilisée pour stocker les buffers,
+// et les command_buffer sont alloués à partir de ça.
+unsafe fn create_command_pool(
+    instance: &Instance,
+    device: &Device,
+    data: &mut AppData,
+) -> Result<()> {
+    let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
+
+    let info = vk::CommandPoolCreateInfo::builder()
+        .flags(vk::CommandPoolCreateFlags::empty())
+        .queue_family_index(indices.graphics);
+
+    data.command_pool = device.create_command_pool(&info, None)?;
+
+    Ok(())
+}
+
+unsafe fn create_command_buffers(device: &Device, data: &mut AppData) -> Result<()> {
+    let allocate_info = vk::CommandBufferAllocateInfo::builder()
+        .command_pool(data.command_pool)
+        .level(vk::CommandBufferLevel::PRIMARY)
+        .command_buffer_count(data.framebuffers.len() as u32);
+
+    data.command_buffers = device.allocate_command_buffers(&allocate_info)?;
+
+    for (i, command_buffer) in data.command_buffers.iter().enumerate() {
+        let inheritance = vk::CommandBufferInheritanceInfo::builder();
+
+        let info = vk::CommandBufferBeginInfo::builder()
+            .flags(vk::CommandBufferUsageFlags::empty()) //Optionnel
+            .inheritance_info(&inheritance); //Optionnel
+
+        device.begin_command_buffer(*command_buffer, &info)?;
+
+        let render_area = vk::Rect2D::builder()
+            .offset(vk::Offset2D::default())
+            .extent(data.swapchain_extent);
+
+        let color_clear_value = vk::ClearValue {
+            color: vk::ClearColorValue {
+                float32: [0.0, 0.0, 0.0, 1.0],
+            },
+        };
+
+        let clear_values = &[color_clear_value];
+        let info = vk::RenderPassBeginInfo::builder()
+            .render_pass(data.render_pass)
+            .framebuffer(data.framebuffers[i])
+            .render_area(render_area)
+            .clear_values(clear_values);
+
+        device.cmd_begin_render_pass(
+            *command_buffer, &info, vk::SubpassContents::INLINE
+        );
+
+        device.cmd_bind_pipeline(
+            *command_buffer, vk::PipelineBindPoint::GRAPHICS, data.pipeline
+        );
+
+        device.cmd_draw(*command_buffer, 3, 1, 0, 0);
+
+        device.cmd_end_render_pass(*command_buffer);
+        device.end_command_buffer(*command_buffer);
+    }
+
 
 
     Ok(())
